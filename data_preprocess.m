68c140adad9fcef6a0885092cd020c7ab476d337
LuYiren <Max@vagvlan536.265.wlan.library.upenn.edu>
2015-11-17 17:02:29 -0500

    update data_preprocess.m

--- data_preprocess.m
+++ data_preprocess.m
@@ -24,11 +24,13 @@ save('test/words_test.mat', 'words_test');
 
 
 %% Read packed data to memory
+% Training 
 load('train/genders_train.mat', 'genders_train');
 load('train/images_train.mat', 'images_train');
 load('train/image_features_train.mat', 'image_features_train');
 load('train/words_train.mat', 'words_train');
 
+%% Testing
 load('test/images_test.mat', 'images_test');
 load('test/image_features_test.mat', 'image_features_test');
 load('test/words_test.mat', 'words_test');
