7fc70133b990279479d8995478623c7b149b3e03
stormmax <luyirenmax@gmail.com>
2015-11-18 16:33:00 -0500

    logistic regression pca 3200

--- pca_lr_sigmoid.m
+++ pca_lr_sigmoid.m
@@ -1,6 +1,7 @@
 % Author: Max Lu
 % Date: Nov 17
 
+
 %% Load the data first, see data_preprocess.m
 load('train/genders_train.mat', 'genders_train');
 load('train/images_train.mat', 'images_train');
@@ -12,6 +13,9 @@ load('test/words_test.mat', 'words_test');
 load('coef.mat', 'coef');
 load('scores.mat', 'scores');
 load('eigens.mat', 'eigens');
+
+% add lib path:
+addpath('./liblinear');
 
 %% 
 tic
@@ -70,13 +74,31 @@ toc
 % Yhat = sigmf(testX*Wmap, [2 0])>0.5;
 
 % % logistic regression
-% X = scores(1:n, 1:2000);
+
+% X = scores(1:n, 1:3200);
 % addpath('./liblinear');
 % disp('logistic regression + cross-validation');
-% [accuracy, Ypredicted, Ytest] = cross_validation(X, Y, 4, @logistic);
+% [accuracy, Ypredicted, Ytest] = cross_validation(X, Y, 8, @logistic);
 % accuracy
 % mean(accuracy)
 % toc
+
+
+
+% Search for the best PC number :
+
+% disp('Search for the best PC number : logistic regression');
+% acc = []
+% for i = 1:6
+% X = scores(1:n, 1:2400+i*200);
+% [accuracy, Ypredicted, Ytest] = cross_validation(X, Y, 4, @logistic);
+% accuracy;
+% 2400+i*200
+% mean(accuracy)
+% acc = [acc;mean(accuracy)];
+% toc
+% end
+% plot(acc);
 
 % LogisticModel
 % model = train(train_y, sparse(train_x), ['-s 0', 'col']);
@@ -95,20 +117,21 @@ toc
 % ylabel('Resubstitution Loss');
 
 % 
-X = scores(1:n, 1:500);
-disp('adaboost + logistic regression + cross-validation');
-[accuracy, Ypredicted, Ytest] = cross_validation(X, Y, 4, @adaboost);
-accuracy
-mean(accuracy)
-toc
+% X = scores(1:n, 1:500);
+% disp('adaboost + logistic regression + cross-validation');
+% [accuracy, Ypredicted, Ytest] = cross_validation(X, Y, 4, @adaboost);
+% accuracy
+% mean(accuracy)
+% toc
 
 
 
+trainX = scores(1:n, 1:3200);
+testX = scores(n+1:size(scores,1), 1:3200);
+model = train(Y, sparse(trainX), ['-s 0', 'col']);
+[Yhat] = predict(ones(size(testX, 1),1), sparse(testX), model, ['-q', 'col']);
+% dlmwrite('submit.txt',Yhat,'\n');
 
-% trainX = scores(1:n, 1:2000);
-% testX = scores(n+1:size(scores,1), 1:2000);
-% model = train(Y, sparse(trainX), ['-s 0', 'col']);
-% [Yhat] = predict(ones(size(testX, 1),1), sparse(testX), model, ['-q', 'col']);
 
 % model = train(train_y, sparse(train_x), ['-s 0', 'col']);
 % [Yhat] = predict(test_y, sparse(test_x), model, ['-q', 'col']);
